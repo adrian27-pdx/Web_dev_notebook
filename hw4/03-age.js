2da5607118f3142b2025b40ab3c721ec7f7b151d
Adrian Perez Gonzalez <adrian27@pdx.edu>
2021-07-30 16:33:16 -0700

    HW4 Part3 Finished

--- hw4/03-age.js
+++ hw4/03-age.js
@@ -1,9 +1,40 @@
 const catculateAge = (birthday) => {
   // Given a date, return the age of the person with that birthday, in years, months, and days
+  var array1 = [];
+  var array2 = []; 
+  let today = new Date();
+
+  array1 = [birthday.getMonth()+1, birthday.getDate(), birthday.getFullYear()];
+  array2 = [today.getMonth()+1, today.getDate(), today.getFullYear()];
+
+  // console.log(array2);
+  // console.log(array1);
+  array2[0] = array2[0]- array1[0];
+  if(array2[0] < 0)
+  {
+    array2[0] = 12 + array2[0];
+    array2[1]--;
+    array2[1] = array2[1]- array1[1];
+  }
+  else
+  {
+    array2[1] = array2[1]- array1[1];
+  }
+  if(array2[1] < 0)
+  {
+    array2[1] = 30 + array2[1];
+    array2[2]--; 
+    array2[2] = array2[2]- array1[2];
+  }
+  else
+  {
+    array2[2] = array2[2]- array1[2];
+  }
+  return "Age: " +array2[2]+ " years, " + array2[0] + " months, " + array2[0]+ " days";
 };
 
 // Date formats
-// new Date('December 17, 1995')
+let birthday = new Date('June 30, 2021')
 // new Date('1995-12-17')
 // new Date(1995, 11, 17)
 // new Date(628021800000)
