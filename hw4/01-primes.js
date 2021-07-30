ad62c70f5ddebf486be6bae9d1315a4c45400eee
Adrian Perez Gonzalez <adrian27@pdx.edu>
2021-07-30 14:24:09 -0700

    Finished HW4 Part 1

--- hw4/01-primes.js
+++ hw4/01-primes.js
@@ -1,5 +1,27 @@
+const isPrime = (current) => {
+  if(current < 2) {
+    return false;
+  }
+
+  for(var count =2; count < current; count++){
+    if(current % count === 0){
+      return false;
+    }
+  }
+  return true;
+}
 const getPrimes = () => {
   // Add your code here
+  var start = 2; 
+  var end = 100; 
+  var flag = 0;
+  for(var current =2; current <= end; ++current)
+  {
+    if(isPrime(current))
+    {
+      console.log(current);
+    }
+  }
 };
 
 getPrimes();
